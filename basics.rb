5ede2ca50f0d71ff5600c07091f4869884948c2b
Daviton <d31630@gmail.com>
2017-02-12 22:13:08 -0500

    exec 1 done

--- basics.rb
+++ basics.rb
@@ -1,16 +1,37 @@
+#1
 first = "David"
 second = "Leal"
 
 puts first + " " + second
 
+#2
 thousands = 6030/1000
 hund = 6030 % 1000 / 100
 tens = 6030 % 100 / 10
 ones = 6030 % 10
 
+#3
 movies = {:lotr => 2000, :xmen => 2012, :sw => 2016}
 puts movies[:lotr]
 puts movies[:xmen]
 puts movies[:sw]
 
+#4
+dates = [2000, 2012, 2016]
+puts dates[0]
+puts dates[1]
+puts dates[2]
 
+#5
+puts 5*4*3*2*1
+puts 6*5*4*3*2*1
+puts 7*6*5*4*3*2*1
+puts 8*7*6*5*4*3*2*1
+
+#6
+puts 2.2*2.2
+puts 1.2*1.2
+puts 9.6*9.6
+
+#7
+#it should have put } insted of )
