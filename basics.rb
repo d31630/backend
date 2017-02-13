5210a8fbb7320fd0184cdfbd4e7fed7d4a166092
Daviton <d31630@gmail.com>
2017-02-12 22:04:46 -0500

    first exerc

---
+++ basics.rb
@@ -0,0 +1,16 @@
+first = "David"
+second = "Leal"
+
+puts first + " " + second
+
+thousands = 6030/1000
+hund = 6030 % 1000 / 100
+tens = 6030 % 100 / 10
+ones = 6030 % 10
+
+movies = {:lotr => 2000, :xmen => 2012, :sw => 2016}
+puts movies[:lotr]
+puts movies[:xmen]
+puts movies[:sw]
+
+
